e4a862ef4edc63899fc045b06f944ca2087a475a
Dasha Khaleneva <Dashloveash@gmail.com>
2020-09-08 22:32:55 +0300

    Калькулятор обновлен

--- calculator.py
+++ calculator.py
@@ -1,37 +1,30 @@
 def calc(N,OP,M):
-    possible_operations = ['+', '-', '*', '/', '^']
+    possible_operations = ['+', '-', '*', '/', '^', '**']
     if OP in possible_operations:
         if OP == '+':
-            print(f'{N}{OP}{M} = {float(N) + float(M)}')
+            result = N + M
         elif OP == '-':
-            print(f'{N}{OP}{M} = {float(N) - float(M)}')
+            result = N - M
         elif OP == '*':
-            print(f'{N}{OP}{M} = {float(N) * float(M)}')
+            result = N * M
         elif OP == '/':
-            print(f'{N}{OP}{M} = {float(N) / float(M)}')
-        elif OP == '^':
-            print(f'{N}{OP}{M} = {float(N) ** float(M)}')
+            result = N / M
+        elif (OP == '^') or (OP == '**'):
+            result = N ** M
+            OP = '^'
+
+        print(result)
     else:
         print(f'Такой операции как {OP} я не знаю')
 
 
-final = True
-while final:
-    print("введите через пробел число, затем операцию, которую вы хотите применить, затем второе число:\n"
-          "для выхода из программы введите exit")
-    z = input()
-    if z == 'exit':
-        final = False
-    else:
-        values = z.split(' ')
-        if len(values) == 3:
-            x, op, y = values
-            try:
-                float(x) and float(y)
-                calc(x, op, y)
-            except:
-                print('Возможно, вы ввели не числа')
-        else:
-            print('Пожалуйста, введите 3 элемента через пробел')
+print("введите число, затем операцию, которую вы хотите применить, затем второе число:\n")
+x, op, y = input(), input(), input()
+
+try:
+    calc(float(x), op, float(y))
+except:
+    print('Возможно, вы ввели не числа')
+
 
 
