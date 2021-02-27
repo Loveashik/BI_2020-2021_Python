db3c0cdcc46716751c7c74de367e9fbcf7842726
Dasha Khaleneva <Dashloveash@gmail.com>
2021-02-27 19:00:47 +0300

    updated generator functions

--- generate_translated_fasta.py
+++ generate_translated_fasta.py
@@ -1,50 +1,51 @@
-from Bio.Seq import Seq
+from Bio import SeqIO
+
+
+'''
+CODON DECODER:
+1. The Standard Code
+2. The Vertebrate Mitochondrial Code
+3. The Yeast Mitochondrial Code
+4. The Mold, Protozoan, and Coelenterate Mitochondrial Code and the Mycoplasma/Spiroplasma Code
+5. The Invertebrate Mitochondrial Code
+6. The Ciliate, Dasycladacean and Hexamita Nuclear Code
+9. The Echinoderm and Flatworm Mitochondrial Code
+10. The Euplotid Nuclear Code
+11. The Bacterial, Archaeal and Plant Plastid Code
+12. The Alternative Yeast Nuclear Code
+13. The Ascidian Mitochondrial Code
+14. The Alternative Flatworm Mitochondrial Code
+16. Chlorophycean Mitochondrial Code
+21. Trematode Mitochondrial Code
+22. Scenedesmus obliquus Mitochondrial Code
+23. Thraustochytrium Mitochondrial Code
+24. Rhabdopleuridae Mitochondrial Code
+25. Candidate Division SR1 and Gracilibacteria Code
+26. Pachysolen tannophilus Nuclear Code
+27. Karyorelict Nuclear Code
+28. Condylostoma Nuclear Code
+29. Mesodinium Nuclear Code
+30. Peritrich Nuclear Code
+31. Blastocrithidia Nuclear Code
+33. Cephalodiscidae Mitochondrial UAA-Tyr Code
+'''
 
 path = '/home/asha/ИБ/python/Generator/test.fa'
 
 
-def generate_translated_fa(file, alp):
+def generate_translated_fa(file: str, alp: int):
     with open(file) as in_file:
-
-        seq_name = ''
-
-        for line in range(sum(1 for l in in_file)):
-            if in_file[line].startswith('>'):
-                seq_name = in_file[line].strip()
-                translated_seq = ''
-            else:
-                x = in_file.readline()
-                while not next(x).startswith('>') or in_file.readline():
-                    translated_seq += Seq(line.strip()).translate(alp)
-                yield seq_name + '\n' + str(translated_seq)
-
-
-
-
-
-
-        # for i,line in enumerate(in_file):
-        #     print(i)
-        #     if i == 300:
-        #         yield describer + '\n' + str(translated_seq)
-        #         break
-        #     elif line[i+1].startswith('>'):
-        #         translated_seq += Seq(line).strip('\n').translate(alp)
-        #         describer = ''
-        #         y=1
-        #     elif not line.startswith('>'):
-        #         translated_seq += Seq(line).strip('\n').translate(alp)
-        #     else:
-        #         describer += str(line).strip()
-        #     if y == 1:
-        #         y = 0
-        #         yield describer + '\n' + str(translated_seq)
+        for cur_seq in SeqIO.parse(in_file, "fasta"):
+            translated_seq = cur_seq.seq.translate(alp)
+            yield translated_seq
 
 
 print('enter a number corresponding to codon table code:')
 codon_table = int(input())
-gener = generate_translated_fa(path, codon_table)
+
 # tests
-for i in range(8):
-    print(next(gener))
-print(list(gener))  # list of seqs with names
+next_fasta_generator = generate_translated_fa(path, codon_table)
+list_fasta_generator = generate_translated_fa(path, codon_table)
+for i in range(10):
+    print(next(next_fasta_generator))
+print(list(list_fasta_generator))  # list of seqs
